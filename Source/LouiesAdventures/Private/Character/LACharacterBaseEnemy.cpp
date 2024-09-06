ffc76fba988f57a9f44bd1e82972b69f7b5bfc53
Zack Shrout <zshrout@gmail.com>
2024-09-05 22:14:28 -0500

    Added enemy character base, and ant enemy

---
+++ Source/LouiesAdventures/Private/Character/LACharacterBaseEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright BunnySoft
+
+
+#include "Character/LACharacterBaseEnemy.h"
+
---
+++ Source/LouiesAdventures/Public/Character/LACharacterBaseEnemy.h
@@ -0,0 +1,17 @@
+// Copyright BunnySoft
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/LACharacterBase.h"
+#include "LACharacterBaseEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOUIESADVENTURES_API ALACharacterBaseEnemy : public ALACharacterBase
+{
+	GENERATED_BODY()
+	
+};
